159367596d5dd9aef6b1ee8a7ef48419a0f7ec35
Sampathar7 <sampathblnkgm@gmail.com>
2024-01-09 21:42:02 +0530

    VideoBackground

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Header from './Header';
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 
 
 // ... (previous imports)
 
 const Browse = () => {
  
+  useNowPlayingMovies();
 
   return (
     <div>
       <Header />
+      <MainContainer/>
+      <SecondaryContainer/>
       {/* You can add rendering logic for the now playing movies here */}
     </div>
   )
